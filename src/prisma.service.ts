2c93772ba7e77d4a49b38800b7d6f1ca702154c5
santiago <linhdevios@gmail.com>
2026-06-18 12:38:55 +0700

    init project

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -1,12 +1,13 @@
-
 import { Injectable } from '@nestjs/common';
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '../generated/prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
   constructor() {
-    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
     super({ adapter });
   }
 }
--- src/user.service.ts
+++ src/user.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 import { Prisma, User } from '../generated/prisma/client';
